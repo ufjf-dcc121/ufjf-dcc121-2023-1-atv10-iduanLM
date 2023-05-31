583e7e96fa94965bcc8682243c67f6105ea94282
IDUAN LUCAS MARTINS <128987724+iduanLM@users.noreply.github.com>
2023-05-30 21:01:46 -0300

    comit final

--- mains.js
+++ mains.js
@@ -1,15 +1,23 @@
-import store from "./store.js";
+import { adicionar,getitems } from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
+form.remover.addEventListener('click',remove);
 
 atualiza();
+
+function remove(){
+    console.log('Remove clickado!');
+    remove();
+    atualiza();
+}
 
 function envia(evento){
     evento.preventDefault();
     console.log('Formulário Enviado!');
     const n = form.valor.value;
-    store.estado.push(n);
+    adicionar(n);
+    form.valor.value = "";
     form.valor.focus();
     atualiza();
 }
@@ -17,9 +25,11 @@ function envia(evento){
 function atualiza(){
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i = 0; i < store.estado.length; i++){
+    const intems = getitems();
+    for(let i = 0; i < items.length; i++){
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = items()[i];
         ol.appendChild(li);
     }
+items().push("boom!");
 }
--- store.js
+++ store.js
@@ -1,2 +1,15 @@
 const store = {estado: [0,1,2,3] };
-export default store;
+
+
+export function adicionar(valor {
+    store.estado.push(valor);
+}
+
+export function remover() {
+    store.estado.pop();
+    
+}
+
+export function getitems() {
+    return [...store.estado];
+}
